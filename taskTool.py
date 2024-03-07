3363f2159b09f55e76c4d5ac7811c17929b90c26
yannyonta <162548963+yannyonta@users.noreply.github.com>
2024-03-07 12:20:56 +0400

    complete_task function

--- taskTool.py
+++ taskTool.py
@@ -1,3 +1,8 @@
 def add_task(task_dict, new_task):
 	task_dict[new_task['id']] = new_task
 	return task_dict
+
+def complete_task(task_dict, task_id):
+    if task_id in task_dict:
+        task_dict[task_id]['completed'] = True
+    return task_dict
