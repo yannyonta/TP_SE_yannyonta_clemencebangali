73cbc66f347744938b96eab8df9ad61f7ec476a6
yannyonta <162548963+yannyonta@users.noreply.github.com>
2024-03-07 12:22:34 +0400

    remove_task function

--- taskTool.py
+++ taskTool.py
@@ -2,6 +2,11 @@ def add_task(task_dict, new_task):
 	task_dict[new_task['id']] = new_task
 	return task_dict
 
+def remove_task(task_dict, task_id):
+    if task_id in task_dict:
+        del task_dict[task_id]
+    return task_dict
+
 def complete_task(task_dict, task_id):
     if task_id in task_dict:
         task_dict[task_id]['completed'] = True
